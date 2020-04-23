31688fd73457db24ace40952eb8a567032c8f9c4
Markus Poeschl <Poeschl@users.noreply.github.com>
2020-04-23 17:53:43 +0100

    Enable git LFS

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 cd build
 git init
+git lfs track "*.zip"
 
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
@@ -12,6 +13,6 @@ git commit -m "Deploy ${TRAVIS_COMMIT}"
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_DEPLOY_KEY}@${GH_REF}" master:dist > /dev/null 2>&1
+git push --force --quiet "https://${GH_DEPLOY_KEY}@${GH_REF}" master:dist
 
 echo 'Deployed on dist branch'
